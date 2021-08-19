d25a32b744adc2fa4cf25ae8881e7e7ea2d2f5a3
Matthew Jaikaran <mattjaikaran@gmail.com>
2021-08-18 23:59:14 -0400

    updated Hero component

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,9 +1,13 @@
-// import { } from 'react-bootstrap'
-
+import { Container, Image } from 'react-bootstrap'
+import styles from './Hero.module.scss'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div
+      className={styles.hero}
+      style={{
+        backgroundImage: 'url(https://via.placeholder.com/1200x400)'
+      }}></div>
   )
 }
 
