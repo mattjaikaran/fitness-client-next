2b17e07a5d55cd9c409fb2a24d49a099f52bcae0
Matthew Jaikaran <mattjaikaran@gmail.com>
2021-08-22 00:00:26 -0400

    updated global link color

--- components/InstructorDetail/InstructorInstagram.jsx
+++ components/InstructorDetail/InstructorInstagram.jsx
@@ -6,7 +6,7 @@ const InstructorInstagram = () => {
     <Container className="text-center p-md-5">
       <h3 className="mt-3">Instagram</h3>
       <h6 className="my-3">
-        <Link className="link-black" href="https://instagram.com/">@josie.norman</Link>| 93.k Followers
+        <Link className="text-black" href="https://instagram.com/">@josie.norman</Link>| 93.k Followers
       </h6>
       <Row>
         <Col xs={6} md={4} className="mb-3">
