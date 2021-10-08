33f485242280c3bb2233cef95b80226b7f6f2ea1
Matthew Jaikaran <mattjaikaran@gmail.com>
2021-10-08 13:20:13 -0400

    added link in slug

--- pages/instructors/[slug].js
+++ pages/instructors/[slug].js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { getAllInstructorIds, getInstructorData } from '@/hooks/instructors'
 
