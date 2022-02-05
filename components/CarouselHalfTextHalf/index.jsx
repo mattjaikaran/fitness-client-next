1e53fe594808c3adfcbb98c15dcbd315d07198eb
Matthew Jaikaran <mattjaikaran@gmail.com>
2022-02-05 15:34:57 -0500

    removed carousel from carouselhalf component

--- components/CarouselHalfTextHalf/index.jsx
+++ components/CarouselHalfTextHalf/index.jsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Carousel from 'react-bootstrap/Carousel'
 import styles from './CarouselHalfTextHalf.module.scss'
 
 const CarouselHalfTextHalf = ({
@@ -15,38 +14,15 @@ const CarouselHalfTextHalf = ({
     <Container fluid>
       <Row>
         <Col className="px-0" lg={6}>
-          {images.length < 2 ? (
-            <div>
-              <Image
-                width={950}
-                height={970}
-                layout="responsive"
-                src={images[0]}
-                alt="First slide"
-              />
-            </div>
-          ) : (
-            <Carousel interval={null} controls={false}>
-              <Carousel.Item>
-                <Image
-                  width={950}
-                  height={970}
-                  layout="responsive"
-                  src={images[0]}
-                  alt="First slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image
-                  width={950}
-                  height={970}
-                  layout="responsive"
-                  src={images[1]}
-                  alt="Second slide"
-                />
-              </Carousel.Item>
-            </Carousel>
-          )}
+          <div>
+            <Image
+              width={950}
+              height={970}
+              layout="responsive"
+              src={images[0]}
+              alt="First slide"
+            />
+          </div>
         </Col>
         <Col className={`mt-sm-3 ${styles.textCol}`} lg={6}>
           <div className={styles.textCopy}>
